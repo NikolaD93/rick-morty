4b8b37a441a2f6fbf902b0b26cccbdce1e920356
Nikola Dojcinovic <goonergalactico@gmail.com>
2024-11-08 16:41:06 +0100

    add theme switcher

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -3,30 +3,32 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// import { queryClient } from '@/lib/react-query';
+import { ThemeProvider } from '@/components/ui/theme-provider';
 import { AppRoutes } from '@/routes';
 
 export const AppProvider = () => {
   const queryClient = new QueryClient();
   const Router = createBrowserRouter(AppRoutes);
   return (
-    <Suspense
-      fallback={
-        <div className="flex h-screen w-screen items-center justify-center">
-          <div
-            className="inline-block h-20 w-20 animate-spin rounded-full border-[3px] border-current border-t-transparent text-blue-600 dark:text-blue-500"
-            role="status"
-            aria-label="loading"
-          >
-            <span className="sr-only">Loading...</span>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Suspense
+        fallback={
+          <div className="flex h-screen w-screen items-center justify-center">
+            <div
+              className="inline-block h-20 w-20 animate-spin rounded-full border-[3px] border-current border-t-transparent text-blue-600 dark:text-blue-500"
+              role="status"
+              aria-label="loading"
+            >
+              <span className="sr-only">Loading...</span>
+            </div>
           </div>
-        </div>
-      }
-    >
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
-        <RouterProvider router={Router} />
-      </QueryClientProvider>
-    </Suspense>
+        }
+      >
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+          <RouterProvider router={Router} />
+        </QueryClientProvider>
+      </Suspense>
+    </ThemeProvider>
   );
 };
